42ea5b69ae7e0c6cee7ebf011ca916e220051a3d
Joao Andre Simioni <joao.simioni@canonical.com>
2023-03-04 20:07:22 -0300

    2023-03-04 01

--- BranchyNet/models/AlexNet.py
+++ BranchyNet/models/AlexNet.py
@@ -239,12 +239,12 @@ class AlexNet(nn.Module):
     def __init__(self, num_classes=10):
         super(AlexNet, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=0),
+            nn.Conv2d(3, 96, kernel_size=3, stride=1, padding=0),
             nn.BatchNorm2d(96),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size = 3, stride = 2))
         self.layer2 = nn.Sequential(
-            nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
+            nn.Conv2d(96, 256, kernel_size=3, stride=1, padding=2),
             nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size = 3, stride = 2))
@@ -261,16 +261,17 @@ class AlexNet(nn.Module):
             nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size = 3, stride = 2))
+        self.flatten = nn.Flatten()
         self.fc = nn.Sequential(
             nn.Dropout(0.5),
-            nn.Linear(9216, 4096),
+            nn.Linear(2304, 1024),
             nn.ReLU())
         self.fc1 = nn.Sequential(
             nn.Dropout(0.5),
-            nn.Linear(4096, 4096),
+            nn.Linear(1024, 1024),
             nn.ReLU())
         self.fc2= nn.Sequential(
-            nn.Linear(4096, num_classes))
+            nn.Linear(1024, num_classes))
         
     def forward(self, x):
         out = self.layer1(x)
@@ -278,8 +279,194 @@ class AlexNet(nn.Module):
         out = self.layer3(out)
         out = self.layer4(out)
         out = self.layer5(out)
-        out = out.reshape(out.size(0), -1)
+        out = self.flatten(out)
         out = self.fc(out)
         out = self.fc1(out)
         out = self.fc2(out)
         return out
+
+
+
+AlexNetCIFAR10Layers = {
+    'backbone' : [
+        nn.Sequential(
+            nn.Conv2d(3, 96, kernel_size=3, stride=1, padding=0),
+            nn.BatchNorm2d(96),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 3, stride = 2)),
+        
+        nn.Sequential(
+            nn.Sequential(
+                nn.Conv2d(96, 256, kernel_size=3, stride=1, padding=2),
+                nn.BatchNorm2d(256),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size = 3, stride = 2)),
+            nn.Sequential(
+                nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(384),
+                nn.ReLU())),
+        
+        nn.Sequential(
+            nn.Sequential(
+                nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(384),
+                nn.ReLU()),
+            nn.Sequential(
+                nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(256),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size = 3, stride = 2)))
+    ],
+    'exits' : [
+        nn.Sequential(
+            nn.Sequential(
+                nn.Conv2d(96, 256, kernel_size=3, stride=1, padding=0),
+                nn.BatchNorm2d(256),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size = 3, stride = 2)),
+            
+            nn.Sequential(nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(256),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size = 3, stride = 2)),
+            
+            nn.Sequential(
+                nn.Flatten(),
+                nn.Linear(1024, num_classes))),
+        
+        nn.Sequential(
+            nn.Sequential(
+                nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(256),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size = 3, stride = 2)),
+            nn.Sequential(
+                nn.Flatten(),
+                nn.Linear(2304, num_classes))),
+
+        nn.Sequential(
+            nn.Flatten(),
+            nn.Sequential(
+                nn.Dropout(0.5),
+                nn.Linear(2304, 1024),
+                nn.ReLU()),
+            nn.Sequential(
+                nn.Dropout(0.5),
+                nn.Linear(1024, 1024),
+                nn.ReLU()),
+            nn.Linear(1024, num_classes))
+    ]
+}
+
+class AlexNetWithExistsCIFAR10(nn.Module):
+    def __init__(self, num_classes=10):
+        super(AlexNetWithExistsCIFAR10, self).__init__()
+
+        self.backbone = nn.ModuleList() 
+        self.exits = nn.ModuleList()
+        self.exit_threshold = torch.tensor([0.5, 0.7], dtype=torch.float32)
+        self.fast_inference_mode = False
+        self.exit_loss_weights = [1.0, 0.5, 0.2]
+
+        self.backbone.append(nn.Sequential(
+            nn.Conv2d(3, 96, kernel_size=3, stride=1, padding=0),
+            nn.BatchNorm2d(96),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 3, stride = 2))
+        )
+
+        self.exits.append(nn.Sequential(
+            nn.Sequential(
+                nn.Conv2d(96, 256, kernel_size=3, stride=1, padding=0),
+                nn.BatchNorm2d(256),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size = 3, stride = 2)),
+            
+            nn.Sequential(nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(256),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size = 3, stride = 2)),
+            
+            nn.Sequential(
+                nn.Flatten(),
+                nn.Linear(1024, num_classes))
+        ))        
+
+        self.backbone.append(nn.Sequential(
+            nn.Sequential(
+                nn.Conv2d(96, 256, kernel_size=3, stride=1, padding=2),
+                nn.BatchNorm2d(256),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size = 3, stride = 2)),
+            nn.Sequential(
+                nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(384),
+                nn.ReLU())
+        ))
+
+        self.exits.append(nn.Sequential(
+            nn.Sequential(
+                nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(256),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size = 3, stride = 2)),
+            nn.Sequential(
+                nn.Flatten(),
+                nn.Linear(2304, num_classes))            
+        ))
+
+        self.backbone.append(nn.Sequential(
+            nn.Sequential(
+                nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(384),
+                nn.ReLU()),
+            nn.Sequential(
+                nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(256),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size = 3, stride = 2))
+        ))
+
+        self.exits.append(nn.Sequential(
+            nn.Flatten(),
+            nn.Sequential(
+                nn.Dropout(0.5),
+                nn.Linear(2304, 1024),
+                nn.ReLU()),
+            nn.Sequential(
+                nn.Dropout(0.5),
+                nn.Linear(1024, 1024),
+                nn.ReLU()),
+            nn.Linear(1024, num_classes)
+        ))
+    
+    def exit_criterion(self, ee_n, x):
+        with torch.no_grad():
+            pk = nn.functional.softmax(x, dim=-1)
+            nc = torch.max(pk)
+            return nc > self.exit_threshold[ee_n]
+
+    @torch.jit.unused #decorator to skip jit comp
+    def _forward_training(self, x):
+        res = []
+        for bb, ee in zip(self.backbone, self.exits):
+            x = bb(x)
+            res.append(ee(x))
+        return res
+
+    def forward(self, x):
+        if self.fast_inference_mode:
+            for ee_n, (bb, ee) in enumerate(zip(self.backbone, self.exits)):
+                x = bb(x)
+                res = ee(x)
+                if self.exit_criterion(ee_n, x):
+                    return [res, 'ee' + ee_n]
+            return [res, 'main']
+        else:
+            return self._forward_training(x)
+
+    def set_fast_inference_mode(self, mode=True):
+        if mode:
+            self.eval()
+        self.fast_inference_mode = mode
+        
