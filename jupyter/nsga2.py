a78d52ba93de595354d6d9056786be3f565a6e28
Joao Simioni <jasimioni@gmail.com>
2023-09-08 16:18:07 +0000

    2023-09-08

---
+++ jupyter/nsga2.py
@@ -0,0 +1,228 @@
+#!/home/ubuntu/ppgia/bin/python3
+
+import os
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import pickle
+import sys
+
+import matplotlib
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+from matplotlib.ticker import MaxNLocator
+from matplotlib import cm
+
+from pymoo.core.problem import ElementwiseProblem
+from pymoo.optimize import minimize
+from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.operators.crossover.sbx import SBX
+from pymoo.operators.mutation.pm import PM
+from pymoo.operators.sampling.rnd import FloatRandomSampling
+from pymoo.termination import get_termination
+
+os.environ['PYTHONUNBUFFERED'] = '1'
+
+directories = {
+    'alexnet'   : '../AlexNet/evaluations/saves/2023-08-20-01-53-10/epoch_19_90.2_91.3.pth/',
+    'mobilenet' : '../MobileNet/evaluations/saves/MobileNetV2WithExits/2023-08-20-05-20-25/epoch_19_89.7_90.9.pth',
+}
+
+'''
+3 objetivos:
+- Acurácia total do sistema
+- Taxa de aceite
+- Tempo médio de inferência
+
+4 parâmetros -> 
+Limiar de normal / ataque na primeira
+Limiar de normal / ataque na segunda
+'''
+
+def get_objectives(df, normal_threshold_exit1, attack_threshold_exit1, normal_threshold_exit2, attack_threshold_exit2):
+    f_n_exit1 = 'y_exit_1 == 0 and cnf_exit_1 >= @normal_threshold_exit1'
+    f_a_exit1 = 'y_exit_1 == 1 and cnf_exit_1 >= @attack_threshold_exit1'
+
+    f_n_exit2 = 'y_exit_2 == 0 and cnf_exit_2 >= @normal_threshold_exit2'
+    f_a_exit2 = 'y_exit_2 == 1 and cnf_exit_2 >= @attack_threshold_exit2'
+
+    exit1_normal = df.query(f_n_exit1)
+    exit1_attack = df.query(f_a_exit1)
+
+    exit2 = df.query(f'not ({f_n_exit1}) and not ({f_a_exit1})')
+
+    exit2_normal = exit2.query(f_n_exit2)
+    exit2_attack = exit2.query(f_a_exit2)
+
+    not_accepted = exit2.query(f'not ({f_n_exit2}) and not ({f_a_exit2})')
+
+    total = df['y'].count()
+
+    exit1_normal_cnt = exit1_normal['y'].count()
+    exit1_attack_cnt = exit1_attack['y'].count()
+    exit2_normal_cnt = exit2_normal['y'].count()
+    exit2_attack_cnt = exit2_attack['y'].count()
+
+    accepted = exit1_normal_cnt + exit1_attack_cnt + exit2_normal_cnt + exit2_attack_cnt
+
+    acceptance_rate = accepted / total
+
+    correct = exit1_normal.query('y == y_exit_1')['y'].count() + \
+              exit1_attack.query('y == y_exit_1')['y'].count() + \
+              exit2_normal.query('y == y_exit_2')['y'].count() + \
+              exit2_attack.query('y == y_exit_2')['y'].count()
+
+    accuracy = correct / accepted
+
+    exit1_total_time = exit1_normal['bb_time_exit_1'].sum() + exit1_normal['exit_time_exit_1'].sum() + \
+                       exit1_attack['bb_time_exit_1'].sum() + exit1_attack['exit_time_exit_1'].sum()
+
+    exit2_total_time = exit2_normal['bb_time_exit_1'].sum() + exit2_normal['bb_time_exit_2'].sum() + exit2_normal['exit_time_exit_2'].sum() + \
+                       exit2_attack['bb_time_exit_1'].sum() + exit2_attack['bb_time_exit_2'].sum() + exit2_attack['exit_time_exit_2'].sum()
+
+    not_accepted_total_time = not_accepted['bb_time_exit_1'].sum() + not_accepted['bb_time_exit_2'].sum() + not_accepted['exit_time_exit_2'].sum()
+
+    total_time = exit1_total_time + exit2_total_time + not_accepted_total_time
+
+    # print(f"Total: {total}")
+    # print(f"exit1_normal_cnt: {exit1_normal_cnt}, exit1_attack_cnt: {exit1_attack_cnt}")
+    # print(f"exit2_normal_cnt: {exit2_normal_cnt}, exit2_attack_cnt: {exit2_attack_cnt}")
+    # print(f"Accepted: {accepted}, Accepted: {total - not_accepted['y'].count()}")
+    # print(f"exit1_total_time: {exit1_total_time:.4f}, exit2_total_time: {exit2_total_time:.4f}, not_accepted_total_time: {not_accepted_total_time:.4f}")
+    # print(f"exit1_rate: {100 * ( exit1_normal_cnt + exit1_attack_cnt ) / total:.2f}, exit2_rate: {100 * ( exit2_normal_cnt + exit2_attack_cnt ) / total:.2f}")
+    # print(f"Accuracy: {100 * accuracy:.2f}, Acceptance: {100 * acceptance_rate:.2f}, Average Time: {1e6 * total_time / total:.2f}")
+
+    return [ accuracy, acceptance_rate, 1e6 * total_time / total ]
+
+def get_values(df, x1, x2, x3, x4):
+    accuracy, acceptance, time = get_objectives(df, x1, x2, x3, x4)
+    return [ 1 - accuracy, 1 - acceptance, (time - min_time) / (max_time - min_time) ]
+
+def plot(network, *cnfs):
+    directory = directories[network]
+
+    datapoints = {
+        'network' : network,
+        'labels' : [],
+        'accuracy_e1' : [],
+        'accuracy_e2' : [],
+        'accuracy_model' : [],
+        'acceptance_e1' : [],
+        'acceptance_e2' : [],
+        'acceptance_model' : [],
+        'time_e1' : [],
+        'time_e2' : [],
+        'time_model' : [],
+    }
+
+    for year in range(2016, 2020):
+        year = f'{year:04d}'
+        for month in range(1, 13):
+            month = f'{month:02d}'
+            glob = f'{year}_{month}'
+            csv = os.path.join(directory, f'{glob}.csv')
+            df = pd.read_csv(csv)
+            
+            accuracy_e1, acceptance_e1, time_e1 = get_objectives(df, 0, 0, 1, 1)
+            accuracy_e2, acceptance_e2, time_e2 = get_objectives(df, 2, 2, 0, 0)
+            accuracy, acceptance, time = get_objectives(df, *cnfs)
+
+            datapoints['labels'].append(glob)
+            datapoints['accuracy_e1'].append(accuracy_e1)
+            datapoints['accuracy_e2'].append(accuracy_e2)
+            datapoints['accuracy_model'].append(accuracy)
+            datapoints['acceptance_e1'].append(acceptance_e1)
+            datapoints['acceptance_e2'].append(acceptance_e2)
+            datapoints['acceptance_model'].append(acceptance)
+            datapoints['time_e1'].append(time_e1)
+            datapoints['time_e2'].append(time_e2)
+            datapoints['time_model'].append(time)
+
+    fig, axs = plt.subplots(2, 1, figsize=(10, 8), layout='constrained')
+    fig.autofmt_xdate(rotation=90)
+    plt.title(f"Accuracy {datapoints['network']} - min cnf: {cnfs}")
+
+    axs[0].plot(datapoints['labels'], datapoints['accuracy_e1'], label='accuracy_e1')
+    axs[0].plot(datapoints['accuracy_e2'], label='accuracy_e2')
+    axs[0].plot(datapoints['accuracy_model'], label='accuracy_model')
+    axs[0].plot(datapoints['acceptance_model'], label='acceptance_model')            
+    axs[0].legend()
+
+    axs[1].plot(datapoints['labels'], datapoints['time_e1'], label='time_e1')
+    axs[1].plot(datapoints['time_e2'], label='time_e2')
+    axs[1].plot(datapoints['time_model'], label='time_model')
+    axs[1].legend()
+    
+    plt.show
+
+class MyProblem(ElementwiseProblem):
+    def __init__(self, df):
+        super().__init__(n_var=4,
+                         n_obj=3,
+                         n_ieq_constr=0,
+                         xl=np.array([ 0.5, 0.5, 0.5, 0.5 ]),
+                         xu=np.array([ 1, 1, 1, 1 ]))
+            
+        self.df = df
+        self.accuracy_e1, self.acceptance_e1, self.min_time = get_objectives(self.df, 0, 0, 1, 1)
+        self.accuracy_e2, self.acceptance_e2, self.max_time = get_objectives(self.df, 2, 2, 0, 0)      
+
+    def _evaluate(self, x, out, *args, **kwargs):
+        accuracy, acceptance, time = get_objectives(self.df, *x)
+        out["F"] = [ 1 - accuracy, 1 - acceptance, (time - self.min_time) / (self.max_time - self.min_time) ]
+        out["G"] = []
+
+def process(directory='alexnet', glob='2016_01'):
+    directory = directories[directory]
+    files = Path(directory).glob(f'*{glob}*')
+    dfs = []
+    for file in sorted(files):
+        dfs.append(pd.read_csv(file))
+
+    df = pd.concat(dfs, ignore_index=True)
+
+    problem = MyProblem(df)
+
+    algorithm = NSGA2(
+        pop_size=100, # 100
+        n_offsprings=80, # 80
+        sampling=FloatRandomSampling(),
+        crossover=SBX(prob=0.9, eta=15),
+        mutation=PM(eta=20),
+        eliminate_duplicates=True
+    )
+
+    termination = get_termination("n_gen", 1000) # 1000
+
+    res = minimize(problem,
+                   algorithm,
+                   termination,
+                   seed=1,
+                   save_history=True,
+                   verbose=True)
+
+    X = res.X
+    F = res.F
+
+    print(f'{directory}')
+    print(f'{glob}')
+
+    print(f'Exit1: {problem.accuracy_e1*100:.2f}% - {problem.acceptance_e1*100:.2f}% - {problem.min_time:.2f}us')
+    print(f'Exit2: {problem.accuracy_e2*100:.2f}% - {problem.acceptance_e2*100:.2f}% - {problem.max_time:.2f}us')
+    print()
+
+    for i in range(len(F)):
+        f = F[i]
+        x = X[i]
+        print(f'{i:02d}: {100 * (1 - f[0]):.2f}% : {100 * (1 - f[1]):.2f}% : {problem.min_time + (f[2] * (problem.max_time - problem.min_time)):.2f}us', end='')
+        print(f'\t{x[0]:.4f} : {x[1]:.4f} : {x[2]:.4f} : {x[3]:.4f}')
+
+    return X, F, problem.min_time, problem.max_time
+
+network = sys.argv[1]
+
+print(f"Processing {network}")
+
+X, F, min_time, max_time = process(network, '2016_0[23]')
+with open(f'{network}_x_f_2016_23.sav', 'wb') as f:
+    pickle.dump([ X, F, min_time, max_time ], f)
